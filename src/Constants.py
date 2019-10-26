dd1f3734eb65a1834c5fee9598f73355ba331bc9
Matthew Worley <mworles@gmail.com>
2019-10-25 17:25:13 -0700

    add Constants.py with scheme of columns to rename for compatibility

---
+++ src/Constants.py
@@ -0,0 +1,9 @@
+COLUMNS_TO_RENAME = {'TeamID': 'team_id',
+                     'LastDayNum': 'last_day',
+                     'CoachName': 'coach_name',
+                     'FirstDayNum': 'first_day',
+                     'TeamNameSpelling': 'name_spelling',
+                     'ConfAbbrev': 'conference',
+                     'WTeamID': 'Wteam',
+                     'LTeamID': 'Lteam',
+                     'WLoc': 'Wloc'}
