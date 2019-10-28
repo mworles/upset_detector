a470c8bc977f1154ca0e4cc99745e983d7afd595
Matthew Worley <mworles@gmail.com>
2019-10-28 13:05:02 -0700

    add season to kp files

--- src/features/features_kp.py
+++ src/features/features_kp.py
@@ -3,13 +3,21 @@ sys.path.append("../")
 import os
 import pandas as pd
 import numpy as np
-from Cleaning import list_files, add_season_column, write_file
+from Cleaning import list_files, get_season, add_season, write_file
 
 print 'running %s' % (os.path.basename(__file__))
 
 directory = '../../data/external/kp/'
 
-# create list of files
-files = list_files(directory, suffix=".csv")
+# create list of file names
+files = [directory + x for x in list_files(directory, suffix=".csv")]
 
-print files
+# use files to get lists of season numbers and dataframes
+seasons = [get_season(x) for x in files]
+dfs = [pd.read_csv(x) for x in files]
+
+# add season column
+data_list = [add_season(x, y) for x, y  in zip(dfs, seasons)]
+
+# combine into single data frame
+df = pd.concat(data_list, sort=True)
