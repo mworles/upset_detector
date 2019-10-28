44d499e688a59bd8ab93eea83de71f1d0340b6d7
Matthew Worley <mworles@gmail.com>
2019-10-28 14:10:34 -0700

    save merged features file

--- src/features/merge_features.py
+++ src/features/merge_features.py
@@ -23,3 +23,6 @@ f = pd.merge(ts, f_coach, how='inner', on=['team_id', 'season'])
 # import kp features, merge
 f_kp = pd.read_csv(dir + 'interim/features_kp.csv')
 f = pd.merge(f, f_kp, how='inner', on=['team_id', 'season'])
+
+# save features data
+write_file(f, '../../data/interim/', 'features_all')
