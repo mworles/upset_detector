5265abd48ecbdbba9b9650a6cfbdfd96279ca26d
Matthew Worley <mworles@gmail.com>
2019-11-13 16:13:56 -0800

    changes for more generalized use

--- src/data/match_pt_id.py
+++ src/data/match_pt_id.py
@@ -1,74 +1,78 @@
 import os
 import pandas as pd
+import numpy as np
 import sys
 sys.path.append("../")
 from Cleaning import write_file, combine_files, clean_school_name, fuzzy_match
 from Constants import COLUMNS_TO_RENAME
 
-def clean_schools(directory):
-    df = combine_files(directory)
-
-    # rows missing value for 'G' column are invalid placeholders
-    df = df.dropna(subset=['G'])
-
-    # isolate data to unique school names
-    df = df[['School']].drop_duplicates()
-
-    # count to compare below for success of matching
-    n_schools = df.shape[0]
-
-    # add clean school name for id matching
-    df['school_clean'] = map(clean_school_name, df['School'].values)
-    
-    return df
-
-def match_school_id(df):
-    
-    # number of schools for later comparison
-    n_schools = df.shape[0]
-    
-    # import team id number data
-    ns = pd.read_csv('../../data/raw/TeamSpellings.csv')
-    # rename some columns for code compatibility
-    ns = ns.rename(columns=COLUMNS_TO_RENAME)
-
-    # join school name to id number in team identifer file 
-    matched = pd.merge(df, ns, how='inner', left_on=['school_clean'],
-                       right_on=['name_spelling'])
-    id_both = matched['team_id'].values
-
-    # dataframe of non-matched school names
-    nm = df[~df['school_clean'].isin(ns['name_spelling'].values)].copy()
-    nm_values = nm['school_clean'].values
-
-    # list of remaining team names
-    rem_teams = ns[~ns['team_id'].isin(id_both)]['name_spelling'].values
-
-    # run function on school column for nonmatched frame
-    nm_names = map(lambda x: fuzzy_match(x, rem_teams), nm_values)
-    nm.loc[:, 'name_spelling'] = nm_names
-    
-    # join team ids using the fuzzy-matched names
-    nm = pd.merge(nm, ns, on='name_spelling', how='inner')
-
-    # combine files
-    all = pd.concat([matched, nm])
-    all = all.drop(columns=['school_clean', 'name_spelling'])
-    
-    # print warning if any school team_ids not identified
-    if n_schools != all.shape[0]:
-        n_left = n_schools - all.shape[0]
-        print 'Warning: %d team ids not joined to school names.' % (n_left)
-    
-    return all
-
-print "running %s" % (os.path.basename(__file__))
-
 directory = '../../data/external/pt/'
 df = combine_files(directory)
-print df.head()
+
+# list of all unique team names
+teams = list(set(list(df['home']) + list(df['road'])))
+
+df = pd.DataFrame({'team': teams})
+df = df[df['team'].notnull()]
+
+# count to compare below for success of matching
+n_rows = df.shape[0]
+
+df['team_clean'] = map(clean_school_name, df['team'].values)
+
+
+# import team id number data
+ns = pd.read_csv('../../data/raw/TeamSpellings.csv')
+# rename some columns for code compatibility
+ns = ns.rename(columns=COLUMNS_TO_RENAME)
+
+nsd = ns.set_index('name_spelling')['team_id'].to_dict()
+
+def get_id(team, id_dict, fuzzy=False):
+    try:
+        tid = id_dict[team]
+        result = (team, tid)
+    except:
+        result = (team, None)
+    if fuzzy==True and result[1] is None:
+        fm = fuzzy_match(x, id_dict.keys())
+        if fm != None:
+            tid = id_dict[fm]
+            result = (team, tid)
+    return result
+
+matches = [get_id(x, nsd) for x in df['team_clean']]
+matches = [x for x in matches if x[1] is not None]
+matched_ids = [x[1] for x in matches]
+matched_teams = [x[0] for x in matches]
+
+teams_remain = [x for x in df['team_clean'] if x not in matched_teams]
+
+nsd_remain = {k: v for (k, v) in nsd.items() if v not in matched_ids}
+matches_remain = [get_id(x, nsd, fuzzy=True) for x in teams_remain]
+matches_remain = [x for x in matches_remain if x[1] is not None]
+
+matches.extend(matches_remain)
+
+md =  {k: v for (k, v) in matches}
+
+df['team_id'] = df['team_clean'].apply(lambda x: get_id(x, md)[1])
+
+print df.sort_values('team_id').tail(10)
+
 
 """
+df['team_id'] = 
+
+
+for t in df['team_clean'].values[0:50]:
+    if t in nsd.keys():
+        tid = nsd[t]
+    else:
+        tid = np.nan
+    print tid
+
+
 schools = clean_schools(directory)
 df = match_school_id(schools)
 df = df.rename(columns={'School': 'ss_team'})
