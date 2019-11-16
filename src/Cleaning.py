7b1da025b1ea977e3e65310bf7d725214c7aa564
Matthew Worley <mworles@gmail.com>
2019-11-15 16:38:16 -0800

    remove id_cols input from set_gameid_index function

--- src/Cleaning.py
+++ src/Cleaning.py
@@ -17,7 +17,7 @@ def combine_files(directory, index_col=False, tag = None):
     
     # if tag given, select file names that include tag
     if tag is not None:
-        files_names = [x for x in file_names if tag in x]
+        file_names = [x for x in file_names if tag in x]
     
     # list of full file names for concatenation
     files = [directory + x for x in file_names]
@@ -66,19 +66,18 @@ def seed_to_numeric(seed):
     new_seed = int(re.sub(r'\D+', '', seed))
     return new_seed
 
-def set_gameid_index(df, id_cols):
+def set_gameid_index(df):
     """
-    Set dataframe index in YYYY_t1##_t2## format to provide each game with
-    a unique identifier.
-    YYYY: Season
+    Set dataframe index in DATE_t1##_t2## format as a unique identifier for each
+    game. Dataframe must have columns of t1_team_id and t2_team_id.
+    DATE: date of game in YYYY_MM_DD format.
     t1##: Lower numerical team ID
     t2##: Higher numerical team ID. 
-    Provides each game with a unique identifier.
     """
-    id_lower = df[id_cols].min(axis=1).astype(str)
-    id_upper = df[id_cols].max(axis=1).astype(str)
-    season = df['season'].astype(str)
-    df['game_id'] = season + '_' + id_lower + '_' + id_upper
+    id_lower = df['t1_team_id'].astype(str)
+    id_upper = df['t2_team_id'].astype(str)
+    date = df['date_id']
+    df['game_id'] = date + '_' + id_lower + '_' + id_upper
     df = df.set_index('game_id')
     return df
 
@@ -133,3 +132,77 @@ def upset_features(df):
         dfr.loc[toswitch, t1_col] = df.loc[toswitch, t2_col]
         dfr.loc[toswitch, t2_col] = df.loc[toswitch, t1_col]
     return dfr
+
+def get_score(row, team_id, score_dict):
+    row_gameid = row.name
+    row_team = row[team_id]
+    team_score = score_dict[row_gameid][row_team]
+    return team_score
+
+def team_id_scores(df):
+    # create dict with key as game identifier, values as team scores
+    score_dict = {}
+    for i, r in df.iterrows():
+        score_dict[i] = {r['wteam']: r['wscore'], r['lteam']: r['lscore']}
+
+    df['t1_score'] = df.apply(lambda x: get_score(x, 't1_team_id', score_dict),
+                              axis=1)
+    df['t2_score'] = df.apply(lambda x: get_score(x, 't2_team_id', score_dict),
+                              axis=1)
+    return df
+    
+def get_t1_win(df, id_cols):
+    df = get_scores(df, id_cols)
+    df['t1_win'] = np.where(df['t1_score'] > df['t2_score'], 1, 0)
+    df = df.drop(columns=['t1_score', 't2_score'])
+    return df
+
+def team_location(row, team):
+    team_col = team + '_team_id'
+    tid = row[team_col]
+    wloc = row['wloc']
+    
+    # team is winner
+    if row['wteam'] == tid:
+        if wloc == 'H':
+            # winner is home, and winner=team, so assign 1
+            team_loc = 1
+        else:
+            # winner is away or neutral, and winner=team, assign 0
+            team_loc = 0
+    
+    # team is not winner
+    else:
+        # winner is away, and winner!=team, so assign 1 
+        if wloc == 'A':
+            team_loc = 1
+        # winner is home or neutral, and winner !=team, so assign 0
+        else:
+            team_loc = 0
+    
+    return team_loc
+
+def get_upsets(idx):
+    df = pd.read_csv('../../data/raw/NCAATourneyCompactResults.csv')
+    df.columns = map(lambda x: x.lower(), df.columns)
+
+    # assigns t1_seed to winning teams, t2_seed to losing teams
+    df = add_seeds('../../data/raw/', df, ['wteamid', 'lteamid'])
+
+    df = set_gameid_index(df, ['wteamid', 'lteamid'])
+    df = df[df.index.isin(idx)]
+    
+    def label_row(row):
+        # absolute seed difference 3 or less, no upset assigned
+        if abs(row.t1_seed - row.t2_seed) <= 3:
+            return np.nan
+        # seed difference 3 or greater, assign 1 for upset
+        elif (row.t1_seed - row.t2_seed) > 3:
+            return 1
+        # assign 0 for no upset
+        else:
+            return 0
+    
+    upset = df.apply(label_row, axis=1)
+    
+    return pd.DataFrame({'upset': upset}, index=df.index)
