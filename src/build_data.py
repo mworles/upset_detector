f6b2f0c28306fa7d01b7410e506bde818610a1b3
Matthew Worley <mworles@gmail.com>
2020-01-10 22:33:57 -0800

    move build_data to top level in src for structural reasons

---
+++ src/build_data.py
@@ -0,0 +1,6 @@
+#import data.scrub_raw
+#import match_school_id
+#import match_kp_id
+#import get_tourney_outcomes
+import data.make_matchups
+#import data.targets
--- src/data/build_data.py
+++
@@ -1,5 +0,0 @@
-import match_school_id
-import match_kp_id
-import get_tourney_outcomes
-import make_matchups
-import targets
