c19589d9f5c3c05d7de08f7baa1fb0b045116c4c
Matthew Worley <mworles@gmail.com>
2020-01-10 22:31:06 -0800

    create raw data map, year constants, and random_seed

--- src/Constants.py
+++ src/Constants.py
@@ -11,6 +11,16 @@ COLUMNS_TO_RENAME = {'TeamID': 'team_id',
 DATA_COLUMN_KEY = {'winner_column': 'WTeamID',
                    'loser_column': 'LTeamID'}
 
-CURRENT_YEAR = 2019
+TEST_YEAR = 2018
+
+MIN_YEAR = 2003
 
 SPLIT_YEARS = [2013, 2014, 2015, 2016, 2017]
+
+RAW_MAP = {'Teams': {'new_name': 'teams',
+                     'cols_rename': {'TeamID': 'team_id',
+                                     'TeamName': 'team_name'}
+                      }
+           }
+
+RANDOM_SEED = 40195
