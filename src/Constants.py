18545357eb30dc62740429af63c7556bb7ade81e
Matthew Worley <mworles@gmail.com>
2020-01-15 12:10:32 -0800

    to combine preprocessing of raw data into one step

--- src/Constants.py
+++ src/Constants.py
@@ -8,19 +8,34 @@ COLUMNS_TO_RENAME = {'TeamID': 'team_id',
                      'LTeamID': 'Lteam',
                      'WLoc': 'Wloc'}
 
-DATA_COLUMN_KEY = {'winner_column': 'WTeamID',
-                   'loser_column': 'LTeamID'}
-
 TEST_YEAR = 2018
 
 MIN_YEAR = 2003
 
 SPLIT_YEARS = [2013, 2014, 2015, 2016, 2017]
 
-RAW_MAP = {'Teams': {'new_name': 'teams',
-                     'cols_rename': {'TeamID': 'team_id',
-                                     'TeamName': 'team_name'}
+RAW_MAP = {}
+
+RAW_MAP['Teams'] = {'new_name': 'teams',
+                     'columns': {'TeamID': 'team_id',
+                                 'TeamName': 'team_name'}
                       }
-           }
+RAW_MAP['TeamSpellings'] = {'new_name': 'team_spellings',
+                            'columns': {'TeamID': 'team_id',
+                                        'TeamNameSpelling': 'name_spelling'}
+                            }
+RAW_MAP['NCAATourneyCompactResults'] = {'new_name': 'ncaa_results',
+                                        'columns': {'WTeamID': 'Wteam',
+                                                    'LTeamID': 'Lteam'}
+                                        }
+RAW_MAP['TeamCoaches'] = {'new_name': 'coaches',
+                          'columns': {'TeamID': 'team_id',
+                                      'FirstDayNum': 'first_day',
+                                      'LastDayNum': 'last_day',
+                                      'CoachName': 'coach_name',
+                                      }
+                          }
 
 RANDOM_SEED = 40195
+
+DATA = '../data/'
