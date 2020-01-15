6dc75ba4924c8d2b79683072c019fb420b5b0b48
Matthew Worley <mworles@gmail.com>
2020-01-15 12:16:38 -0800

    create module for pre-processing raw data

--- src/data/scrub.py
+++ src/data/scrub.py
@@ -1,16 +1,11 @@
 import pandas as pd
-from Constants import RAW_MAP
 from Clean import write_file
-
-files = RAW_MAP.keys()
-
-name = files[0]
 
 def scrub_file(name, file_map):
     """Convert file using map of new column and file names."""
     file = '../data/raw/' + name + '.csv'
     df = pd.read_csv(file)
-    df = df.rename(columns=file_map[name]['cols_rename'])
+    df = df.rename(columns=file_map[name]['columns'])
     df.columns = df.columns.str.lower()
     return df
 
@@ -20,4 +15,7 @@ def scrub_write(name, file_map):
     name_new = file_map[name]['new_name']
     write_file(df, data_out, name_new, keep_index=False)
 
-scrub_write(name, RAW_MAP)
+def scrub_files(file_map):
+    files = file_map.keys()
+    for f in files:
+        scrub_write(f, file_map)
