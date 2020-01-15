697f5ba85efb74a5c9c662d7a8a5afae63d8f184
Matthew Worley <mworles@gmail.com>
2020-01-15 12:12:48 -0800

    to incorporate new modules for data cleaning

--- src/build_data.py
+++ src/build_data.py
@@ -1,6 +1,19 @@
-#import data.scrub_raw
-#import match_school_id
-#import match_kp_id
-#import get_tourney_outcomes
-import data.make_matchups
+import Constants
+import data
+from features.features_coach import get_coach
+dir = '../data/'
+
+# first cleaning step to change variable names and case for consistency
+#data.scrub.scrub_files(Constants.RAW_MAP)
+"""
+# create files matching school names to numeric identifiers
+ss = data.match.match_schools(dir)
+kp = data.match.match_kp(dir)
+data.match.combine_id(dir, [ss, kp])
+"""
+# create data on wins and games for each prior tournament
+from data import get_tourney_outcomes
+
+data.Clean.write_file(get_coach(), dir + 'features/', 'features_coach')
+#import data.make_matchups
 #import data.targets
