66ea946e07cc0dc949e35e4c6a4581c19947e474
Matthew Worley <mworles@gmail.com>
2020-01-16 12:57:12 -0800

    add seeds, changes to data and feature packages

--- src/build_data.py
+++ src/build_data.py
@@ -4,21 +4,28 @@ import features
 #from features.features_coach import get_coach
 
 dir = '../data/'
-"""
+
 # pre-process raw data
 data.scrub.scrub_files(Constants.RAW_MAP)
 
+"""
 # create files matching school names to numeric identifiers
 ss = data.match.match_schools(dir)
 kp = data.match.match_kp(dir)
 data.match.combine_id(dir, [ss, kp])
 
-
-
 # create data on wins and games for each prior tournament
-from data import get_tourney_outcomes
+data.Generate.tourney_outcomes(dir)
+
+# create coach features
+features.features_coach.get_coach(dir)
+
+# team seeds as features
+features.Create.team_seeds(dir)
 """
-features.features_coach.write_coach(dir)
+
+# merge features
+
 
 #import data.make_matchups
 #import data.targets
