77170136a8039c5deb7a98b14fb5ea69b3f0ef15
Matthew Worley <mworles@gmail.com>
2020-01-17 09:48:29 -0800

    create functions for adding games, matchups, and targets

--- src/data/Generate.py
+++ src/data/Generate.py
@@ -9,6 +9,7 @@ This script requires `pandas`. It imports the custom Clean module.
 """
 
 import pandas as pd
+import numpy as np
 import Clean
 
 def tourney_outcomes(dir):
@@ -42,3 +43,130 @@ def tourney_outcomes(dir):
     
     # write file
     Clean.write_file(df, dir + 'interim/', 'tourney_outcomes')
+
+def set_games(dir):
+    """Create data containing features for both teams in matchup."""
+    r = pd.read_csv(dir + 'scrub/ncaa_results.csv')
+    s = sk = pd.read_csv(dir + 'scrub/seasons.csv')
+    df = pd.merge(r, s, on='season', how='inner')
+    
+    # add string date column to games
+    df['date_id'] = df.apply(Clean.game_date, axis=1)
+    
+    # create outcome-neutral team identifiers
+    team_cols = ['wteam', 'lteam']
+    df = Clean.convert_team_id(df, team_cols, drop=True)
+    
+    # keep only necessary info to add features
+    keep = ['season', 't1_team_id', 't2_team_id', 'date_id']
+    df = df[keep]
+    
+    return df
+    
+def add_features(dir, df):
+    # import features data
+    file_feat = dir + 'features/team_features.csv'
+    feat = pd.read_csv(file_feat)
+    
+    # cols to exclude when renaming features
+    exc = ['team_id', 'season']
+    # feature columns to rename
+    tcols = [x for x in list(feat.columns) if x not in exc]
+    
+    t1dict = {}
+    t2dict = {}
+    
+    for t in tcols:
+        t1dict[t] = 't1_' + t
+        t2dict[t] = 't2_' + t
+    
+    
+    # create data for both teams with t1 and t2 prefix
+    t1 = feat.copy().rename(columns=t1dict)
+    t2 = feat.copy().rename(columns=t2dict)
+    
+
+    # merge for team 1
+    merge_on=['season', 'team_id']
+    df1 = pd.merge(df, t1, left_on=['season', 't1_team_id'], right_on=merge_on,
+                   how='inner')
+    df1 = df1.drop('team_id', axis=1)
+    
+    df2 = pd.merge(df1, t2, left_on=['season', 't2_team_id'], right_on=merge_on,
+                   how='inner')
+    df2 = df2.drop('team_id', axis=1)
+    
+    #df2 = Clean.set_gameid_index(df2)
+    return df2
+
+def make_matchups(dir):
+    matchups = set_games(dir)
+    df = add_features(dir, matchups)
+    
+    # set unique game index
+    df = Clean.set_gameid_index(df)
+        
+    # save data
+    Clean.write_file(df, dir + 'processed/', 'matchups', keep_index=True)
+
+def get_upsets(dir, df):
+    
+    def label_upset(row):
+        
+        if row['seed_abs'] >= 4:
+            if row['t1_seed_dif'] > 0:
+                if row['t1_marg'] > 0:
+                    upset = 1
+                else:
+                    upset = 0
+            else:
+                if row['t1_marg'] > 0:
+                    upset = 0
+                else:
+                    upset = 1
+        else:
+            upset = np.nan
+        
+        return upset
+
+    # seeds data
+    s = pd.read_csv(dir + '/processed/matchups.csv', index_col=0)
+    s = s[['t1_seed', 't2_seed']]
+    # absolute seed difference
+    s['t1_seed_dif'] = s['t1_seed'] - s['t2_seed']
+    s['seed_abs'] = s['t1_seed_dif'].apply(abs)
+    # merge with scores
+    mrg = pd.merge(df, s, left_index=True, right_index=True)
+    
+    mrg['upset'] = mrg.apply(label_upset, axis=1)
+    
+    return mrg
+
+def score_targets(dir, df):
+    # binary indicator of 1 if t1_team won
+    df['t1_win'] = np.where(df['t1_score'] > df['t2_score'], 1, 0)
+
+    # score margin 
+    df['t1_marg'] = df['t1_score'] - df['t2_score']
+    
+    # upset labels
+    df = get_upsets(dir, df)
+    
+    # keep target columns
+    df = df.loc[:, ['t1_win', 't1_marg', 'upset']]
+    
+    return df
+    
+def make_targets(dir):
+    file = dir + '/scrub/ncaa_results.csv'
+    df = pd.read_csv(file)
+    
+    # created ordered, outcome_neutral team identifier
+    df = Clean.convert_team_id(df, ['wteam', 'lteam'], drop=False)
+    # create unique game identifier and set as index
+    df = Clean.set_gameid_index(df)
+    # add column indicating score for each team
+    scores = Clean.team_scores(df)
+    df = score_targets(dir, scores)
+    
+    Clean.write_file(df, dir + '/processed/', 'targets', keep_index=True)
