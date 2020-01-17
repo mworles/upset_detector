c3878f7afa688b5a355bcfc26f729bc34746faf0
Matthew Worley <mworles@gmail.com>
2020-01-17 09:53:04 -0800

    account for pre-processed files without renamed columns

--- src/data/scrub.py
+++ src/data/scrub.py
@@ -1,11 +1,12 @@
 import pandas as pd
-from Clean import write_file
+import Clean
 
 def scrub_file(name, file_map):
     """Convert file using map of new column and file names."""
     file = '../data/raw/' + name + '.csv'
     df = pd.read_csv(file)
-    df = df.rename(columns=file_map[name]['columns'])
+    if 'columns' in file_map[name].keys():
+        df = df.rename(columns=file_map[name]['columns'])
     df.columns = df.columns.str.lower()
     return df
 
@@ -13,7 +14,7 @@ def scrub_write(name, file_map):
     df = scrub_file(name, file_map)
     data_out = '../data/scrub/'
     name_new = file_map[name]['new_name']
-    write_file(df, data_out, name_new, keep_index=False)
+    Clean.write_file(df, data_out, name_new, keep_index=False)
 
 def scrub_files(file_map):
     files = file_map.keys()
