6ec6e687fcd48b93ec4d7639dd62c48ffa736449
Matthew Worley <mworles@gmail.com>
2020-01-17 09:46:52 -0800

    add additional features, make matchups and targets

--- src/build_data.py
+++ src/build_data.py
@@ -1,31 +1,32 @@
 import Constants
 import data
 import features
-#from features.features_coach import get_coach
 
+# specify top data directory
 dir = '../data/'
 
 # pre-process raw data
-data.scrub.scrub_files(Constants.RAW_MAP)
+data.Scrub.scrub_files(Constants.RAW_MAP)
 
-"""
 # create files matching school names to numeric identifiers
-ss = data.match.match_schools(dir)
-kp = data.match.match_kp(dir)
-data.match.combine_id(dir, [ss, kp])
+data.Match.create_key(dir)
 
 # create data on wins and games for each prior tournament
 data.Generate.tourney_outcomes(dir)
 
-# create coach features
-features.features_coach.get_coach(dir)
-
-# team seeds as features
+# create features
+# coach
+features.Create.coach_features(dir)
+# team seeds
 features.Create.team_seeds(dir)
-"""
+# ratings
+features.Create.team_ratings(dir)
 
 # merge features
+features.Create.merge_features(dir)
 
+# combine features for both teams in matchups
+data.Generate.make_matchups(dir)
 
-#import data.make_matchups
-#import data.targets
+# generate targets for prediction
+data.Generate.make_targets(dir)
