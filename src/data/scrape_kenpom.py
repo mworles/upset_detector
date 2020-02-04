01b012ceb221ca4108e075c002a13b6a9ab82c32
Matthew Worley <mworles@gmail.com>
2020-02-04 09:16:39 -0800

    add fix to remove data in 'span' elements

--- src/data/scrape_kenpom.py
+++ src/data/scrape_kenpom.py
@@ -3,7 +3,6 @@ import requests
 import datetime
 import Transfer
 
-# Create a method that parses a given year and spits out a raw dataframe
 def get_ratings(url):
     """
     Imports raw data from kenpom website.
@@ -29,17 +28,42 @@ def get_ratings(url):
                 repeats[s] +=1
     
     tr = t.tbody.find_all('tr')
+    
     tds = [x.find_all('td') for x in tr]
-    rows = [[x.getText() for x in tdl] for tdl in tds]
+    tds = [x for x in tds if len(x) > 0]
+    
+    def no_span(x):
+        has_span = td.find('span')
+        if has_span is None:
+            return x.getText()
+        else:
+            return None
+
+    def clean_text(x):
+        x = x.replace('+', '')
+        return x
+
+    rows = [[no_span(td) for td in row] for row in tds]
+    rows = [[x for x in row if x is not None] for row in rows]
+    rows = [[clean_text(x) for x in row] for row in rows]
+    
+    date = str(datetime.datetime.now().date())
+    [r.insert(0, date) for r in rows]
+    
+    names.insert(0, 'date')
     
     rows.insert(0, names)
-    
+
     return rows
 
 url = 'http://kenpom.com/index.php'
 rows = get_ratings(url)
-rows[0].insert(0, 'date')
 
-date = str(datetime.datetime.now().date())
+dbt = Transfer.DBTable('ratings', rows)
 
-[x.insert(0, date) for x in rows[1:]]
+
+
+#dba = Transfer.DBAssist()
+#dba.connect('../../aws.config')
+#dba.create_table('ratings', rows)
+#dba.insert_rows('ratings', rows[:10])
