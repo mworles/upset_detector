b38a37f747d0ebdbda1934166e9d86e52156238d
Matthew Worley <mworles@gmail.com>
2020-02-04 09:17:42 -0800

    add custom classes to make operations modular

--- src/data/Transfer.py
+++ src/data/Transfer.py
@@ -12,8 +12,6 @@ def extract_data(file):
         reader = csv.reader(csvfile)
         rows = [x for x in reader]
     
-    len_data = len(rows)
-    
     return rows
 
 def file_rows(name, directory):
@@ -38,13 +36,17 @@ def column_type(col):
     if any(col_texts):
         col_type = """ TEXT """
     else:
-        col_type = """ NUMERIC """
+        col_type = """ DECIMAL(10, 5) """
     return col_type
+
+def clean_name(name):
+    name_clean = name.replace('-', '_')
+    return name_clean
 
 def query_column(col):
     """Given a vector of data with header, returns portion of query
     with column name and data type for MSQL create table."""
-    col_name = col[0]
+    col_name = clean_name(col[0])    
     col_type = column_type(col[1:])
     column_details = "".join([col_name, col_type])
     return column_details
@@ -54,6 +56,7 @@ def query_columns(column_list):
     qcs = ", ".join(column_details)
     return qcs
 
+    
 def query_create_table(name, rows):
     """Returns full MYSQL query needed to create table from flat .csv file."""
     
@@ -73,6 +76,7 @@ def format_value(x):
     """Format string value for MYSQL insert statement."""
     if no_digits(x):
         x = x.replace("'", r"\'")
+        x = x.replace(".", "")
         xf = r"""'%s'""" % (x)
     else:
         xf = """%s""" % (x)
@@ -101,7 +105,7 @@ def row_as_query(row):
     q_values = "".join(["(", row_s, ")"])
     return q_values
 
-def query_insert_rows(rows):
+def query_insert_rows(table_name, rows):
     """Return full MYSQL insert query for all rows to insert."""
     # list of separate row values for mysql insert
     row_queries = map(lambda x: row_as_query(x), rows)
@@ -113,17 +117,7 @@ def query_insert_rows(rows):
     
     return query_insert
 
-def insert_rows(table_name, rows, cursor):
-    """Given table name with list of rows, insert all rows."""
-    # obtain full insert query for all rows
-    query_insert = query_insert_rows(rows)
-    try:
-        result = cursor.execute(query_insert)
-        return True
-    except:
-        return False
-
-def table_exists(table_name, cursor):
+def check_table(table_name, cursor):
     qp = """SELECT COUNT(*) FROM information_schema.tables WHERE table_name = """ 
     qtn = """'%s'""" % (table_name)
     q_full = "".join([qp, qtn])
@@ -143,7 +137,7 @@ def create_and_insert(table_name, directory, cursor):
     # first row is column headers
     print '%s rows extracted' % (len(rows) - 1)
     
-    if table_exists(table_name, cursor) == False:
+    if check_table(table_name, cursor) == False:
         q_create = query_create_table(table_name, rows)
         cursor.execute(q_create)
     
@@ -159,26 +153,76 @@ def transfer_directory(directory, cursor):
     
     return l_tables
 
+def get_col_info(col):
+    try:
+        col_f = [str(float(x)) for x in col]
+        dec_splits = [x.split('.') for x in col_f]
+        imax = max([len(x[0]) for x in dec_splits])
+        dmax = max([len(x[1]) for x in dec_splits])
+        col_type = " DECIMAL (%s, %s) " % (imax, dmax)
+    except:
+        col_type = """ VARCHAR (64) """
 
-config = ConfigParser.ConfigParser()
-config.readfp(open('../../aws.config'))
-server = config.get('RDS', 'server')
-database = config.get('RDS', 'database')
-uid = config.get('RDS', 'uid')
-code = config.get('RDS', 'code')
+    return col_type
 
-# establish connection to database and create cursor
-conn = pyodbc.connect('DRIVER=MySQL ODBC 8.0 ANSI Driver;'
-                      'SERVER='+server+';'
-                      'DATABASE='+database+';'
-                      'UID='+uid+';'
-                      'PWD='+code+';'
-                      'charset=utf8mb4;')
-cursor = conn.cursor()
+class DBTable():
+    
+    def __init__(self, name, data):
+        self.name = name
+        self.column_names = data[0]
+        self.rows = data[1:]
+        #self.columns = map(list, zip(*self.rows))
+        self.data_dict = {t[0]: {'values': t[1:]} for t in map(list, zip(*data))}
 
-# create tables and insert
-l_tables = transfer_directory(datdir, cursor)
+    
+    def get_col_type(self):
+        for name in self.column_names():
+            dd[name]['col_type'] = get_col_info(dd[name]['values'])
 
-# commit changes and close connection
-conn.commit()
-conn.close()
+class DBAssist():
+    def __init__(self):
+        self.conn = None
+        self.cursor = None
+
+    def connect(self, config_file):
+        self.config_file = config_file
+        parser = ConfigParser.ConfigParser()
+        parser.readfp(open('../../aws.config'))
+        server = parser.get('RDS', 'server')
+        database = parser.get('RDS', 'database')
+        uid = parser.get('RDS', 'uid')
+        code = parser.get('RDS', 'code')
+        self.conn = pyodbc.connect('DRIVER=MySQL ODBC 8.0 ANSI Driver;'
+                                   'SERVER='+server+';'
+                                   'DATABASE='+database+';'
+                                   'UID='+uid+';'
+                                   'PWD='+code+';'
+                                   'charset=utf8mb4;')
+        self.cursor = self.conn.cursor()
+    
+    def extract_data(self, file):
+        """Extract and return all rows from data file as list of lists."""
+        
+        with open(file) as csvfile:
+            reader = csv.reader(csvfile)
+            rows = [x for x in reader]
+        
+        len_data = len(rows)
+        
+        return rows
+
+    def create_table(self, table_name, rows):
+        table_exists = check_table(table_name, self.cursor)
+        if table_exists == False:
+            query_create = query_create_table(table_name, rows)
+            self.cursor.execute(query_create)
+            self.conn.commit()
+
+    def insert_rows(self, table_name, rows):
+        """Given table name with list of rows, insert all rows."""
+        # obtain full insert query for all rows
+        rows_data = format_rows(rows[1:])
+        query_insert = query_insert_rows(table_name, rows_data)
+        print query_insert
+        self.cursor.execute(query_insert)
+        self.conn.commit()
