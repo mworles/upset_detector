9ccd8d6759b8290a0ab7f5e5f032419529ac1be8
Matthew Worley <mworles@gmail.com>
2020-02-05 10:29:07 -0800

    script for scraper job

---
+++ src/data/run_scrape.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+import datetime
+import Transfer
+import Scrapers
+
+
+
+url = 'http://kenpom.com/index.php'
+rows = Scrapers.team_ratings(url)
+
+
+dbt = Transfer.DBTable('ratings', rows)
+dbt.setup_table()
+
+dba = Transfer.DBAssist()
+dba.connect('../../aws.config')
+dba.create_table(dbt)
+dba.insert_rows(dbt)
+
+dba.close()
