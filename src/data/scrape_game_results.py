fb1179e048943bc1fa5ba7cee0a36bce74dc2982
Matthew Worley <mworles@gmail.com>
2020-02-15 15:34:52 -0800

    fix to grab date before today for completed games

--- src/data/scrape_game_results.py
+++ src/data/scrape_game_results.py
@@ -2,7 +2,10 @@ import Scrapers, Clean, Transfer
 import datetime
 import Transfer
 
-date = datetime.datetime.now().date().strftime('%Y/%m/%d')
+date = datetime.datetime.now()
+# to run overnight, get results for yesterday's date
+date = date - datetime.timedelta(days=1)
+date = date.strftime('%Y/%m/%d')
 df = Scrapers.game_results(date)
 rows = Clean.dataframe_rows(df)
 Transfer.insert('game_scores', rows)
