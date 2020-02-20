7e103990d6a75acbd5d1673cb1292302040f8868
Matthew Worley <mworles@gmail.com>
2020-02-20 10:52:22 -0800

    in progress function to clean tcpalm box for possessions estimate

--- src/test.py
+++ src/test.py
@@ -19,6 +19,7 @@ def tcp_conversion(df):
 
 dba = Transfer.DBAssist()
 dba.connect()
+"""
 table = dba.return_table('game_scores')
 df = pd.DataFrame(table[1:], columns=table[0])
 df = Match.id_from_name(datdir, df, 'team_tcp', 'away_team', drop=False)
@@ -38,6 +39,34 @@ df = df.dropna(how='any', subset=['t1_score', 't2_score', 't1_loc', 't2_loc'])
 df['t1_score'] = df['t1_score'].astype(int)
 df['t2_score'] = df['t2_score'].astype(int)
 df = Ratings.location_adjustment(df)
+"""
+tbox = dba.return_table('game_box')
+box = pd.DataFrame(tbox[1:], columns=tbox[0])
+
+def split_makes_attempts(elem):
+    try:
+        elem_split = elem.split('-')
+        return (elem_split[0], elem_split[1])
+    except:
+        return None
+
+for team in ['home_', 'away_']:
+    old_col = team + 'FGMA'
+    makes_attempts = map(split_makes_attempts, box[old_col].values)
+    new_col = team + 'fga'
+    box[new_col] = [x[1] if x is not None else None for x in makes_attempts]
+    old_col = team + 'FTMA'
+    makes_attempts = map(split_makes_attempts, box[old_col].values)
+    new_col = team + 'fta'
+    box[new_col] = [x[1] if x is not None else None for x in makes_attempts]
+
+# remove missing rows missing any posession cols
+pos_list = ['_fta', '_fga', '_OFF', '_TO']
+pos_list = [x for x in pos_list if not '_TOT' in x]
+pos_cols = [x for x in box.columns if any(ele in x for ele in pos_list)]
+print pos_cols
+box = box.dropna(subset=pos_cols, how='any')
+
 
 # need to merge in box scores
 
