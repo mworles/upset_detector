e3a876f2bbcb7bc2bf693c3ffbdfaedfbfab189b
Matthew Worley <mworles@gmail.com>
2020-02-25 10:16:33 -0800

    add/edit functions to clean all odds data sources

--- src/data/Odds.py
+++ src/data/Odds.py
@@ -2,10 +2,9 @@ import json
 import pandas as pd
 import numpy as np
 import calendar
-import Clean
-import Generate
+import Clean, Generate, Match
 
-def odds_games(year_file):
+def oddsportal_games(year_file):
     
     year_games = []
 
@@ -23,11 +22,11 @@ def odds_games(year_file):
 
     return year_games
 
-def parse_odds(datdir):
+def parse_oddsportal(datdir):
     all_games = []
 
     year_files = Clean.list_of_files(datdir)
-    all_games = [odds_games(file) for file in year_files]
+    all_games = [oddsportal_games(file) for file in year_files]
     all_flat = [game for year in all_games for game in year]
 
     encode_cell = lambda x: x.encode('ascii', 'ignore')
@@ -36,7 +35,7 @@ def parse_odds(datdir):
 
     return all_flat
 
-def odds_dates(date_values):
+def oddsportal_dates(date_values):
         
     month_key = {v: k for k,v in enumerate(calendar.month_abbr)}
 
@@ -49,7 +48,7 @@ def odds_dates(date_values):
     year = [x[2] for x in date_spl]
     day = [x[1] for x in date_spl]
 
-    date_new = ["_".join(x) for x in zip(year, mon, day)]
+    date_new = ["/".join(x) for x in zip(year, mon, day)]
     
     return date_new
 
@@ -62,46 +61,37 @@ def format_odds(x):
     
     return result
 
-def get_id_key(datdir, df, name_col):
-    tid = pd.read_csv(datdir + '/interim/id_key.csv')
-    # remove if missing or duplicated
-    tid = tid[['team_id', name_col]]
-    tid = tid.rename(columns={name_col: 'name'})
-    tid = tid[~tid['name'].duplicated()]
-    dict_tid = tid.set_index('name').to_dict('index')
-    return dict_tid
+def current_odds(df):
+    gbc = ['game_date', 'team_1', 'team_2']
+    dfgb = df.groupby(gbc)
+    dfgb = dfgb.apply(lambda x: x.sort_values(['timestamp'], ascending=False))
+    dfgb = dfgb.reset_index(drop=True).groupby(gbc).head(1)
+    return dfgb
 
-def order_odds(row, id_key):    
-    try:
-        t1 = id_key[row['team_1']]['team_id']
-    except:
-        t1 = np.nan
-    try:
-        t2 = id_key[row['team_2']]['team_id']
-    except:
-        t2 = np.nan
-    
-    # lower numeric id first or second
-    lower_i = [t1, t2].index(min([t1, t2]))
-    
-    if lower_i == 0:
-        return [row['game_date'], t1, t2, row['odds1'], row['odds2']]
+def get_odds_dicts(df):
+    grid = df[['odds1', 'odds2', 'team_1_id', 'team_2_id']].values
+    game_dict = lambda x: {x[2]: x[0], x[3]: x[1]}
+    l_games = map(game_dict, grid)
+    return l_games
 
-    else:
-        return [row['game_date'], t2, t1, row['odds2'], row['odds1']]
+def team_odds(df, game_dicts):
+    t1_id = df['t1_team_id'].values
+    t2_id = df['t2_team_id'].values
+    df['t1_odds'] = [dict[team] for dict, team in zip(game_dicts, t1_id)]
+    df['t2_odds'] = [dict[team] for dict, team in zip(game_dicts, t2_id)]
+    return df
 
 
-def clean_odds(datdir):
+
+def clean_oddsportal(datdir):
     oddsdir = datdir + "/external/odds/"
-    data = parse_odds(oddsdir)
+    data = parse_oddsportal(oddsdir)
     col_names = ['date', 'team_1', 'team_2', 'odds1', 'odds2']
     df = pd.DataFrame(data, columns=col_names)
 
-    date_new = odds_dates(df['date'].values)
-    df['game_date'] = date_new
+    date_new = oddsportal_dates(df['date'].values)
     df = df.drop('date', axis=1)
-    df = df.sort_values('game_date', ascending=True)
-
+    df['date'] = date_new
 
     odds1 = df['odds1'].apply(format_odds).values
     odds2 = df['odds2'].apply(format_odds).values
@@ -109,20 +99,20 @@ def clean_odds(datdir):
     df['odds1'] = odds1
     df['odds2'] = odds2
 
-    df = df[['game_date', 'team_1', 'team_2', 'odds1', 'odds2']]
+    df = Match.id_from_name(df, 'team_oddsport', 'team_1', drop=False)
+    df = Match.id_from_name(df, 'team_oddsport', 'team_2', drop=False)
+    
+    l_games = get_odds_dicts(df)
+    df = Generate.convert_team_id(df, ['team_1_id', 'team_2_id'], drop=False)
+    df = team_odds(df, l_games)
 
-    id_key = get_id_key(datdir, df, 'team_oddsport')
-    result = df.apply(lambda x: order_odds(x, id_key), axis=1).tolist()
-    cols_new = ['game_date', 't1_team_id', 't2_team_id', 't1_odds', 't2_odds']
-    df_new = pd.DataFrame(result, columns=cols_new)
+    df = df.dropna(subset=['t1_team_id', 't2_team_id'])  
     
-    df_new = df_new.dropna(subset=['t1_team_id', 't2_team_id'])  
+    df['t1_team_id'] = df['t1_team_id'].astype(int)
+    df['t2_team_id'] = df['t2_team_id'].astype(int)
     
-    df_new['t1_team_id'] = df_new['t1_team_id'].astype(int)
-    df_new['t2_team_id'] = df_new['t2_team_id'].astype(int)
+    df = Generate.set_gameid_index(df, date_col='date', full_date=True, drop_date=False)
     
-    df_new = Generate.set_gameid_index(df_new, date_col='game_date',
-                                       full_date=True, drop_date=False)
-    
-
-    Clean.write_file(df_new, datdir + '/interim/', 'odds', keep_index=True)
+    keep_cols = ['date', 't1_team_id', 't2_team_id', 't1_odds', 't2_odds']
+    df = df[keep_cols].sort_values('date').reset_index()
+    return df
