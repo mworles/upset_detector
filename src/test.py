b7f4469d3cac82cdb1e4f3d5ce48e3dd64fd0407
Matthew Worley <mworles@gmail.com>
2020-02-26 16:23:03 -0800

    separating and creating functions for historical and incoming data

--- src/test.py
+++ src/test.py
@@ -3,77 +3,91 @@ import Constants
 import pandas as pd
 import numpy as np
 import math
-
-# location of data directory from constants module
-#datdir = Constants.DATA
-#Clean.scrub_files(Constants.RAW_MAP)
-
-# create database table
-#Transfer.create_from_schema('games_for_ratings', 'data/schema.json')
-# create and insert games for ratings up to season 2019
-"""
-df = Ratings.games_ratings(Constants.DATA, year=2019)
-rows = Transfer.dataframe_rows(df)
-Transfer.insert('games_for_ratings', rows, at_once=False)
-"""
+import json
 
 
-"""
-# clean and insert new games for ratings
-#date = "2020/02/19"
-df = Ratings.game_box_for_ratings()
-rows = Transfer.dataframe_rows(df)
-Transfer.insert('games_for_ratings', rows, at_once=False) 
-"""
+def db_create(schema_file):
+    with open(schema_file, 'r') as f:
+        schema = json.load(f)
+    for k in schema.keys():
+        Transfer.create_from_schema(k, 'data/schema.json')
 
+def db_build(datdir, ratings=False):
+    df = Ratings.games_ratings(Constants.DATA)
+    rows = Transfer.dataframe_rows(df)
+    Transfer.insert('games_for_ratings', rows, at_once=False)
+    
+    df = Ratings.game_box_for_ratings()
+    rows = Transfer.dataframe_rows(df)
+    Transfer.insert('games_for_ratings', rows, at_once=False) 
 
-#2Transfer.create_from_schema('ratings_at_day', 'data/schema.json')
+    df = pd.concat([df, df])
+    years = pd.unique(df['season']).tolist()
+    
+    if ratings != False:
+        if __name__ == '__main__':
+            for year in years:
+                Ratings.run_year(year=year, n_iters=15)
+    
+    # clean from oddsportal and insert
+    df = Odds.clean_oddsportal(datdir)
+    rows = Transfer.dataframe_rows(df)
+    Transfer.insert('odds_clean', rows, at_once=False) 
 
-if __name__ == '__main__':
-    Ratings.run_year(year=2019, n_iters=15)
+    # create blended clean spreads table
+    df = Spreads.blend_spreads(datdir)
+    rows = Transfer.dataframe_rows(df)
+    Transfer.insert('spreads_clean', rows, at_once=False) 
 
-
-
-"""
-# compute and insert ratings up to most recent date in games_for_ratings
-df = Ratings.run_day(year = 2020, n_iters=5)
-rows = Transfer.dataframe_rows(df)
-#Transfer.create_from_schema('ratings_at_day', 'data/schema.json')
-Transfer.insert('ratings_at_day', rows)
-"""
-
-
-"""
-# clean from oddsportal and insert
-#Transfer.create_from_schema('odds_clean', 'data/schema.json')
-datdir = Constants.DATA
-df = Odds.clean_oddsportal(datdir)
-rows = Transfer.dataframe_rows(df)
-Transfer.insert('odds_clean', rows, at_once=False) 
-"""
-
+def db_update(date):
+    # add games for ratings for date
+    df = Ratings.game_box_for_ratings(date)
+    rows = Transfer.dataframe_rows(df)
+    Transfer.insert('games_for_ratings', rows, at_once=False) 
+    
+    # pull all exising games for ratings from current season
+    df = Transfer.return_data('games_for_ratings')
+    year = float(date.split('/')[0])
+    df = df[df['season'] == year]
+    # compute ratings up to date
+    df = Ratings.run_day(df)
+    # insert current ratings to table
+    rows = Transfer.dataframe_rows(df)
+    Transfer.insert('ratings_at_day', rows)
 
 """
 # clean and insert current odds
-df = Odds.odds_vi(date=None)
+df = Odds.odds_vi()
 rows = Transfer.dataframe_rows(df)
-Transfer.insert('odds_clean', rows, at_once=False) 
-"""
+Transfer.insert('odds_current', rows, at_once=False, delete=True)
 
-
-"""
-# create blended clean spreads table in mysql
-datdir = Constants.DATA
-df = Spreads.blend_spreads(datdir)
-Transfer.create_from_schema('spreads_clean', 'data/schema.json')
-rows = Transfer.dataframe_rows(df)
-Transfer.insert('spreads_clean', rows, at_once=False) 
-"""
-
-
-"""
 # clean and insert current spreads
-df = Spreads.spreads_vi(date=None)
+df = Spreads.spreads_vi()
 rows = Transfer.dataframe_rows(df)
-Transfer.insert('odds_clean', rows, at_once=False) 
+Transfer.insert('spreads_current', rows, at_once=False, delete=True)
 """
+dba = Transfer.DBAssist()
+dba.connect()
+odds = dba.return_df('odds_current').set_index('game_id')
+spreads = dba.return_df('spreads_current').set_index('game_id')
+ratings = dba.return_df('ratings_at_day')
+most_recent = max(ratings['date'].values)
+ratings = ratings[ratings['date'] == most_recent]
+ratings = ratings.drop(columns=['season', 'date'])
+ratings.columns = [x.replace('team_', '') for x in ratings.columns]
+t1 = ratings.copy()
+t1.columns = ['t1_' + x for x in t1.columns]
+t2 = ratings.copy()
+t2.columns = ['t2_' + x for x in t2.columns]
+
+spreads = spreads.drop(columns=['date', 't1_team_id', 't2_team_id'])
+mrg = pd.merge(odds, spreads, left_index=True, right_index=True).reset_index()
+mrg = pd.merge(mrg, t1, left_on='t1_team_id', right_on='t1_id', left_index=True, 
+               how='inner')
+mrg = pd.merge(mrg, t2, left_on='t2_team_id', right_on='t2_id', left_index=True, 
+               how='inner')
+mrg.set_index('game_id')
+mrg = Clean.add_team_name(mrg, datdir='../data/')
+
+
+print mrg[['team_1', 'team_2', 't1_eff_marg', 't2_eff_marg', 't1_spread', 't1_odds', 't2_odds']].head(30)
