eafd806efb8df411ecedfb047b101442b1f65eff
Matthew Worley <mworles@gmail.com>
2020-03-01 22:03:28 -0800

    add current date to script

--- src/data/update_daily.py
+++ src/data/update_daily.py
@@ -1,3 +1,9 @@
 import Updater
+import datetime
+
+date = datetime.datetime.now()
+# will schedule to run overnight, get results for yesterday's date
+date = date - datetime.timedelta(days=1)
+date = date.strftime('%Y/%m/%d')
 
 Updater.update_day('2020/02/26')
