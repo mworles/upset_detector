ebe4d76b64df500e610432da7e3ae43b6d586742
Matthew Worley <mworles@gmail.com>
2020-03-01 21:53:28 -0800

    create and update team_home data and game_info with scores

--- src/data/Updater.py
+++ src/data/Updater.py
@@ -1,10 +1,11 @@
-import Transfer, Ratings, Odds, Spreads
+import Transfer, Ratings, Odds, Spreads, Clean, Generate
+from features import Create
 import pandas as pd
 import numpy as np
 import math
 import json
-
-
+import datetime
+import features
 
 def create(schema_file):
     with open(schema_file, 'r') as f:
@@ -17,6 +18,10 @@ def create(schema_file):
 
 def build(datdir, ratings=False):
     """Run once to insert data gathered from flat files."""
+    
+    # pre-process raw data
+    Clean.scrub_files(Constants.RAW_MAP, out='mysql')
+    
     df = Ratings.games_ratings(datdir)
     rows = Transfer.dataframe_rows(df)
     Transfer.insert('games_for_ratings', rows, at_once=False)
@@ -25,7 +30,7 @@ def build(datdir, ratings=False):
     rows = Transfer.dataframe_rows(df)
     Transfer.insert('games_for_ratings', rows, at_once=False) 
 
-    df = pd.concat([df, df])
+    df = Transfer.return_data('games_for_ratings')
     years = pd.unique(df['season']).tolist()
     
     if ratings != False:
@@ -43,6 +48,27 @@ def build(datdir, ratings=False):
     rows = Transfer.dataframe_rows(df)
     Transfer.insert('spreads_clean', rows, at_once=False) 
 
+    # get team home indicators
+    # stored results
+    for table in ['reg_results', 'ncaa_results', 'nit_results']:
+        df = Transfer.return_data(table)
+        df = Create.results_home(df)
+        results = Transfer.dataframe_rows(df)
+        Transfer.insert('team_home', results, at_once=False, create=False,
+                        delete=False)
+
+    # scraped results
+    results = Create.game_home(date=None)
+    Transfer.insert('team_home', results, at_once=False, create=False,
+                    delete=False)
+
+    # team info for past seasons
+    df = Generate.convert_past_games()
+    df = Generate.make_game_info(df)
+    rows = Transfer.dataframe_rows(df)
+    Transfer.insert('game_info', rows, at_once=False)    #
+
+    
 def update_day(date):
     """Run once daily after all games have ended."""
     # add games for ratings for date
@@ -50,7 +76,7 @@ def update_day(date):
     rows = Transfer.dataframe_rows(df)
     Transfer.insert('games_for_ratings', rows, at_once=False) 
     
-    # pull all exising games for ratings from current season
+    # pull all existing games for ratings from current season
     year = float(date.split('/')[0])
     modifier = "WHERE season = %s" % (str(year))
     df = Transfer.return_data('games_for_ratings', modifier=modifier)
@@ -72,6 +98,26 @@ def update_day(date):
     spreads = Spreads.spreads_vi(date)
     rows = Transfer.dataframe_rows(spreads)
     Transfer.insert('spreads_clean', rows, at_once=False)
+    
+    # insert rows to game_info table for day's games
+    mod = """where date = '%s'""" % (date)
+    df = Transfer.return_data('game_scores', modifier=mod)
+    df = Generate.convert_game_scores(df)
+    df = Generate.make_game_info(df)
+    rows = Transfer.dataframe_rows(df)
+    Transfer.insert('game_info', rows, at_once=False)
+    
+    # get team location for day's games, insert rows to team_home
+    rows = features.Create.game_home(date)
+    Transfer.insert('team_home', rows, at_once=False)
+
+    # get team location for next week's scheduled games
+    start = Clean.date_plus(date, 1)
+    end = Clean.date_plus(start, 5)
+    dates = Clean.date_range(start, end)
+    rows = Create.game_home(dates)
+    Transfer.insert("team_home_current", rows, at_once=True, delete=True)
+
 
 def update_current():
     """Run as frequently as desired to update current matchups."""
@@ -89,3 +135,47 @@ def update_current():
     mrg = mrg[['game_id', 'date', 't1_team_id', 't2_team_id']]
     rows = Transfer.dataframe_rows(mrg)
     Transfer.insert('matchups_current', rows, at_once=False, delete=True)
+
+def features_to_matchup(df_mat, df_feat, merge_cols=[]):
+    # copy team features and merge for each team in matchup
+    # df_feat must have 'team_id' column as team identifer
+    t1_merge = ['t1_team_id'] + merge_cols
+    t2_merge = ['t2_team_id'] + merge_cols
+    
+    t1 = df_feat.copy()
+    t1.columns = ['t1_' + x if x not in merge_cols else x for x in t1.columns]
+    t2 = df_feat.copy()
+    t2.columns = ['t2_' + x if x not in merge_cols else x for x in t2.columns]
+
+    mrg = pd.merge(df_mat, t1, left_on=t1_merge, right_on=t1_merge, how='inner')
+    mrg = pd.merge(mrg, t2, left_on=t2_merge, right_on=t2_merge, how='inner')
+    
+    return mrg
+
+
+def current_matchup_features():
+    
+    df = Transfer.return_data('matchups_current')
+    df = df.sort_values('game_id')
+    dates = list(set(df['date']))
+    dates = [r"'" + x + r"'" for x in dates]
+    dates_in = ", ".join(dates)
+    
+    modifier = "WHERE date = (SELECT max(date) from ratings_at_day)"
+    ratings = Transfer.return_data('ratings_at_day', modifier=modifier)
+    ratings = ratings.drop(columns=['season', 'date'])
+    ratings.columns = [x.replace('team_', '') for x in ratings.columns]
+    ratings = ratings.rename(columns={'id': 'team_id'})
+
+    modifier = "WHERE date IN (%s)""" % (dates_in)
+    home = Transfer.return_data('team_home', modifier=modifier)
+    home = home.drop(columns=['game_id'])
+    
+    df = features_to_matchup(df, ratings)
+    df = features_to_matchup(df, home, merge_cols=['date'])
+    
+    cols_remove = ['game_id', 'date','t1_team_id', 't2_team_id']
+
+    df = df.drop(cols_remove, axis=1).copy()
+    
+    return df
