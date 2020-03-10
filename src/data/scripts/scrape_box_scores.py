1ca28e5b2152ae4253ac523bb6b8ae4b20cc6cc7
Matthew Worley <mworles@gmail.com>
2020-03-10 14:59:42 -0700

    remove test date

--- src/data/scripts/scrape_box_scores.py
+++ src/data/scripts/scrape_box_scores.py
@@ -2,13 +2,10 @@ from src.data import scrapers
 from src.data import Transfer
 import datetime
 
-"""
 date = datetime.datetime.now()
 # will schedule to run overnight, get results for yesterday's date
 date = date - datetime.timedelta(days=1)
 date = date.strftime('%Y/%m/%d')
-"""
-date = '2020/03/06'
 
 game_scores = scrapers.games.game_scores(date)
 
