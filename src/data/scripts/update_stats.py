65644bc96cf8caa1e117c95dea5113aff12b0639
Matthew Worley <mworles@gmail.com>
2020-03-11 17:17:21 -0700

    finish transform of game box data

--- src/data/scripts/update_stats.py
+++ src/data/scripts/update_stats.py
@@ -1,8 +1,14 @@
 from src.data import features
 from src.data import Transfer
+from src.data import Generate
+from src.data import Match
 
 df = Transfer.return_data('game_box')
-df = features.team.split_teams(df)
-print df.sort_values(['team', 'date']).head(20)
-df = features.team.clean_box(df)
-print df.sort_values(['team', 'date']).head(20)
+st = features.team.split_teams(df)
+cb = features.team.clean_box(st)
+sbt = Generate.games_by_team(cb)
+sbt = sbt.rename(columns={'team_id': 'team_name'})
+sbt = Match.id_from_name(sbt, 'team_tcp', 'team_name')
+
+
+#df = prep_season(season, subset='ncaa'
