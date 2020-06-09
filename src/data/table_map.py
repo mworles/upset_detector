4167db11abbbd3adc80373c3b0be0d8c824c5c8d
Matthew Worley <mworles@gmail.com>
2020-06-08 23:05:08 -0700

    add new tables to table map

--- src/data/table_map.py
+++ src/data/table_map.py
@@ -23,3 +23,50 @@ KEY['MNCAATourneyDetailedResults'] = {'new_name': 'ncaa_results_dtl',
                                       'columns': {'WTeamID': 'Wteam',
                                                   'LTeamID': 'Lteam'}
                                       }
+
+KEY['MTeams'] = {'new_name': 'teams',
+                 'columns': {'TeamID': 'team_id',
+                             'TeamName': 'team_name'}
+                 }
+
+KEY['MTeamSpellings'] = {'new_name': 'team_spellings',
+                         'columns': {'TeamID': 'team_id',
+                                     'TeamNameSpelling': 'name_spelling'}
+                         }
+
+KEY['MRegularSeasonCompactResults'] = {'new_name': 'reg_results',
+                                       'columns': {'WTeamID': 'Wteam',
+                                                   'LTeamID': 'Lteam'}
+                                       }
+
+KEY['MRegularSeasonDetailedResults'] = {'new_name': 'reg_results_dtl',
+                                        'columns': {'WTeamID': 'Wteam',
+                                                    'LTeamID': 'Lteam'}
+                                        }
+
+KEY['MNCAATourneySeeds'] = {'new_name': 'seeds',
+                            'columns': {'TeamID': 'team_id'}
+                            }
+
+KEY['MSeasons'] = {'new_name': 'seasons'}
+
+KEY['MGameCities'] = {'new_name': 'game_cities',
+                      'columns': {'WTeamID': 'WTeam',
+                                  'LTeamID': 'LTeam',
+                                  'CRType': 'game_cat',
+                                  'CityID': 'city_id'}
+                               }
+
+KEY['Cities'] = {'new_name': 'cities',
+                 'columns': { 'CityID': 'city_id'}
+                 }
+
+KEY['TeamGeog'] = {'new_name': 'team_geog',
+                   'columns': { 'lat': 'latitude',
+                               'lng': 'longitude'}
+                   }
+
+KEY['TourneyGeog'] = {'new_name': 'tourney_geog',
+                      'columns': {'lat': 'latitude',
+                                  'lng': 'longitude'}
+                      }
