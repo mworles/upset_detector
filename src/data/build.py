18e431b4946c8e60ca0fb1cda8ea414d8e097cd6
Matthew Worley <mworles@gmail.com>
2020-06-08 23:04:53 -0700

    move s3 data functions to clean module, add code to compile ratings

--- src/data/build.py
+++ src/data/build.py
@@ -1,36 +1,16 @@
-import boto3
-import io
 import pandas as pd
-import table_map
-import transfer
+from src.data import transfer
+from src.data import clean
 from src.features import team
-
-def s3_data(file_name):
-    s3_resource = boto3.resource('s3')
-    bucket_name = 'worley-upset-detector-public'
-    
-    s3_object = s3_resource.Object(bucket_name=bucket_name, key=file_name)
-    response = s3_object.get()
-    data_string = response['Body'].read().decode('utf-8')
-    data = pd.read_csv(io.StringIO(data_string))
-    return data
-
-
-def convert_raw_file(file_name, key=table_map.KEY):
-    df = s3_data(file_name)
-    raw_name = file_name.replace('.csv', '')
-    df = df.rename(columns=key[raw_name]['columns'])
-    # make any other columns lowercase for consistency
-    df.columns = df.columns.str.lower()
-
-    return df
+from src.features import ratings
+import table_map
 
 dba = transfer.DBAssist()
 
-"""
 for table in table_map.KEY:
     file_name = '{}.csv'.format(table)
-    df = convert_raw_file(file_name)
+    df = clean.s3_data(file_name)
+    df = clean.convert_raw_file(file_name)
     table_name = table_map.KEY[table]['new_name']
 
     try:
@@ -39,7 +19,10 @@ for table in table_map.KEY:
         dba.create_from_data(table_name, df)
 
     dba.insert_rows(table_name, df)
-"""
+
 dba.create_from_schema('tourney_success')
 df = team.tourney_performance()
 dba.insert_rows('tourney_success', df)
+ratings = ratings.compile()
+dba.create_from_data('ratings', ratings)
+dba.insert_rows('ratings', ratings)
